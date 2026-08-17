0fb702bb61935a52e93cf52b3ee880c436ef2761
Santi <santinoagos1234@gmail.com>
2026-08-17 13:35:25 -0300

    Agregar diagnostico de Node

---
+++ diagnostico.js
@@ -0,0 +1,11 @@
+console.log("Diagnostico de Node.js");
+console.log("----------------------");
+
+console.log("Version de Node:", process.version);
+console.log("Plataforma:", process.platform);
+
+console.log("Argumentos:");
+console.log(process.argv);
+
+console.log("Variable de entorno USERNAME:");
+console.log(process.env.USERNAME);
